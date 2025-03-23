a7786395c161703dde48fe39f36f2000fee7e3da
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-03-23 05:14:17 +0100

    Implement class IsReady

--- src/is-ready.ts
+++ src/is-ready.ts
@@ -6,8 +6,52 @@
 
 // .............................................................................
 export class IsReady {
-  /** Example instance for test purposes */
+  constructor() {}
+
+  static get instance(): IsReady {
+    return new IsReady();
+  }
+
   static get example(): IsReady {
     return new IsReady();
   }
+
+  get state(): boolean {
+    return this._state;
+  }
+
+  set state(value: boolean) {
+    this._state = value;
+    if (value) {
+      this._callbacks.forEach((callback) => {
+        callback();
+      });
+    }
+
+    this._callbacks = [];
+  }
+
+  get isReady(): Promise<void> {
+    return this._isReady();
+  }
+
+  // ######################
+  // Private
+  // ######################
+
+  private _state: boolean = false;
+
+  private _isReady(): Promise<void> {
+    return new Promise<void>((resolve) => {
+      if (this._state) {
+        resolve();
+      } else {
+        this._callbacks.push(() => {
+          resolve();
+        });
+      }
+    });
+  }
+
+  private _callbacks: Array<() => void> = [];
 }
--- test/is-ready.spec.ts
+++ test/is-ready.spec.ts
@@ -8,10 +8,56 @@ import { describe, expect, it } from 'vitest';
 
 import { IsReady } from '../src/is-ready';
 
-
 describe('IsReady', () => {
-  it('should validate a template', () => {
-    const isReady = IsReady.example;
-    expect(isReady).toBeDefined();
+  describe('instance', () => {
+    it('returns an instance of the IsReady', () => {
+      const isReady = IsReady.instance;
+      expect(isReady).toBeInstanceOf(IsReady);
+    });
+  });
+
+  describe('example', () => {
+    it('returns an example object', () => {
+      const example = IsReady.example;
+      expect(example).toBeInstanceOf(IsReady);
+    });
+  });
+
+  describe('get state, set state', () => {
+    it('returns the state', () => {
+      const isReady = IsReady.instance;
+      expect(isReady.state).toBe(false);
+
+      isReady.state = true;
+      expect(isReady.state).toBe(true);
+    });
+  });
+
+  describe('isReady', () => {
+    it('returns a promise that resolves when state is true', async () => {
+      let isReady0 = false;
+      let isReady1 = false;
+
+      const isReady = IsReady.instance;
+      expect(isReady.state).toBe(false);
+      isReady.isReady.then(() => {
+        isReady0 = true;
+      });
+
+      isReady.isReady.then(() => {
+        isReady1 = true;
+      });
+
+      expect(isReady0).toBe(false);
+      expect(isReady1).toBe(false);
+
+      isReady.state = true;
+
+      // Wait for the promise to resolve
+      await isReady.isReady;
+
+      expect(isReady0).toBe(true);
+      expect(isReady1).toBe(true);
+    });
   });
 });
